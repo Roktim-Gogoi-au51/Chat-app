a7e6fa866bd32cfa97b56433d3571e0bb7862e3e
Roktim-Gogoi-au51 <gogoiroktim97@gmail.com>
2023-05-21 15:56:59 +0530

    chat-server added

--- client/src/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import userReducer from "./reducers/userReducers";
-
-const store = createStore(userReducer, applyMiddleware(thunk))
-
-export default store;
